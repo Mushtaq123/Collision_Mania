219d9148322e389feba362648a1d43133225d1e5
Vishal <vish.sethi123@gmail.com>
2014-11-28 17:53:26 +0530

    UI updation

--- src/com/example/collision_mania/MainActivity.java
+++ src/com/example/collision_mania/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -19,6 +20,16 @@ public class MainActivity extends Activity {
 		start1 = (Button)findViewById(R.id.button2);
 		startTri = (Button)findViewById(R.id.button3);
 		startTri1 = (Button)findViewById(R.id.button4);
+		
+		TranslateAnimation anim = new TranslateAnimation(-500,0, 0, 0); //first 0 is start point, 150 is end point horizontal
+		anim.setDuration(3000); // 1000 ms = 1second
+		anim.setFillAfter(true);
+		
+		start.startAnimation(anim);
+		start1.startAnimation(anim);
+		startTri.startAnimation(anim);
+		startTri1.startAnimation(anim);
+		
 		
 		start.setOnClickListener(new View.OnClickListener() {
 			
